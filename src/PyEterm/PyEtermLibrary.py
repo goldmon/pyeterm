63fce117028789b5a98e48598c4726f1aee31303
goldmon <43789188@qq.com>
2017-11-07 14:16:20 +0800

    修改版本低报错及不用配置文件

--- src/PyEterm/PyEtermLibrary.py
+++ src/PyEterm/PyEtermLibrary.py
@@ -126,13 +126,18 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     logging.info("hello")
     
-    config = ConfigParser.ConfigParser()
-    config.readfp(codecs.open('../config.cgi', 'r', 'utf-8-sig'))
+    #config = ConfigParser.ConfigParser()
+    #config.readfp(codecs.open('../config.cgi', 'r', 'utf-8-sig'))
     
-    server = config.get("eterm", "server")
-    port = config.getint("eterm", "port")
-    username = config.get("eterm", "username")
-    password = config.get("eterm", "password")
+    #server = config.get("eterm", "server")
+    #port = config.getint("eterm", "port")
+    #username = config.get("eterm", "username")
+    #password = config.get("eterm", "password")
+
+    server = "123.57.4.86"
+    port = 350
+    username = "zk"
+    password = "zk"
     
     el = PyEtermLibrary()
     el.connect(server, port)   
